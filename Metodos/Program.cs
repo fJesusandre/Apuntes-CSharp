9d6bc072fa2881feb240eae8eb6d6f1012265f8e
Jesus Andre <fjesusandre@gmail.com>
2023-01-12 18:52:18 -0600

    Ambito y sobrecarga de metodos. Video 12

--- Metodos/Program.cs
+++ Metodos/Program.cs
@@ -4,6 +4,13 @@ namespace Metodos
 {
     internal class Program
     {
+        //Si una variable o constante es declarada dentro de una clase pero fuera de un metodo se dice que tiene "Ambito de clase". Ambito local dentro de un metodo.
+
+        //A estas variables o constantes son denominadas "Campos de clase".
+        int numeroClase1 = 5;
+        int numeroClase2 = 7;
+
+
         static void Main(string[] args)
         {
             //sumarNumeros(10, 13);// Nota: No se puede llamar un metodo en un main estatico si el metodo no es estatico tambien.
@@ -25,6 +32,15 @@ namespace Metodos
             return num1 + num2;
         }
 
+        //Expression-bodied =>
         static int divideNumeros(int num1, int num2) => num1 / num2; // Manera de escribir o SIMPLIFICAR metodos con una linea de codigo como los anteriores.
+        
+        void primerMetodo()
+        {
+            Console.WriteLine(numeroClase2 + numeroClase1);// Cualquier metodo puede acceder a los campos de clase si se encuentre dentro de la misma clase.
+            // No importa si los campos de clase son declaradas al principio o al final. Son como variable globales.
+        }
+        
+        // Sobrecarga de metodos: Cuando dos o mas metodos tienen el mismo nombre dentro de una clase.
     }
 }
