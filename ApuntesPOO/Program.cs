b66a4a345eceb5d33128ac1ed3565904ef8a7487
Jesus Andre <fjesusandre@gmail.com>
2023-03-03 23:28:57 -0600

    Creacion de ApuntesPOO

---
+++ ApuntesPOO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class ParadigmaPOO{
+    static void Main(string[] args){
+
+        Circulo circulo1 = new Circulo(); //Declaracion, Creacion de objeto de tipo circulo e iniciacion del objeto o instanciar una clase. 
+        Console.WriteLine( circulo1.calculoArea(3));
+
+    }
+
+    class Circulo{
+        double pi = 3.1416; // Propiedad; Campo de clase.
+        public double calculoArea(int radio){
+            
+            return pi*radio*radio;
+        }
+
+    }
+}
+
