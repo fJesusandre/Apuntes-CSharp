afc1ce91908193a038c1fb3f0bb89feecac8e9ae
Jesus Andre <fjesusandre@gmail.com>
2023-03-06 19:19:32 -0600

    Encapsulacion, convenciones y  metodos de acceso. V29

--- ApuntesPOO/Program.cs
+++ ApuntesPOO/Program.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 
 class ParadigmaPOO{
+
+    // los identificadores "public" deben de ir en notacion PascalCase y los "private" en camelCase.
+
     static void Main(string[] args){
 
         Circulo circulo1 = new Circulo(); //Declaracion, Creacion de objeto de tipo circulo e iniciacion del objeto o instanciar una clase. 
-        Console.WriteLine( circulo1.calculoArea(3));
-        Console.WriteLine(circulo1.color = "azul");
-
+        Console.WriteLine( circulo1.CalculoArea(3));
+        Console.WriteLine(circulo1.Color = "azul");// Esto es una mala practica, hay que usar metodos de acceso.
+        circulo1.DeterminarCantidad(2);
+        Console.WriteLine($"Cantidad de circulos creados: {circulo1.ConsultarCantidad()}");
     }
 
     class Circulo{
-        double pi = 3.1416; // Propiedad; Campo de clase.
-        public string color = "";
-        public double calculoArea(int radio){
+        private const double pi = 3.1416; // Propiedad; Campo de clase. Esta encapsulada, es recomendable usar private. 
+        public string Color = "Sin color";
+        private int Cantidad = 1;
+        public double CalculoArea(int radio){
             
             return pi*radio*radio;
+        }
+
+    // Metodos de acceso
+        public void DeterminarCantidad(int cantidadNueva){
+            if(cantidadNueva > 1) Cantidad = cantidadNueva;
+        }
+
+        public int ConsultarCantidad(){
+            return Cantidad;
         }
 
     }
