8e159ba79278d592c89aa14424a16f965a8dd9bc
Jesus Andre <fjesusandre@gmail.com>
2023-03-10 14:17:45 -0600

    Construtores. v30

--- ApuntesPOODos/Program.cs
+++ ApuntesPOODos/Program.cs
@@ -4,7 +4,43 @@ namespace ApuntesPOODos{
 
     internal class Program{
         static void Main(string[] args){
-            
+            Coche autoOne = new Coche(); //instanciacion. Se dio un estado inicial al autoOne. Se hace uso del constructor.
+
+            Console.WriteLine(autoOne.getInfoCoche());
+
+            Coche autoTwo = new Coche(4000.5, 1500.3);
+
+            Console.WriteLine(autoTwo.getInfoCoche());
         }
+    }
+
+    class Coche{
+
+        // Constructor. Lleva el mismo nombre de la clase y no lleva void ni regresa ningun valor.
+        public Coche(){
+            ruedas = 4;
+
+            largo = 2300.5;
+
+            ancho = 1000;
+        }
+
+        // Sobrecarga de constructores.
+        public Coche(double largoEsp, double anchoEsp){
+            ruedas = 4;
+            largo = largoEsp;
+            ancho = anchoEsp;
+        }
+
+        // Metodo de acceso. Getter.
+        public string getInfoCoche(){
+            return $"Informacion del coche:\n\nCantidad de ruedas: {ruedas}\nDimensiones del coche: {largo}x{ancho}";
+        }
+
+        private int ruedas;
+        private double largo;
+        private double ancho;
+        private bool clima;
+        private string tapiceria;
     }
 }
