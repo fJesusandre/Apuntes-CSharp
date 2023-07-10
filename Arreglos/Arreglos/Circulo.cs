46bd128f372028276ed2a17d2cbe9b866a9564f9
Fernando <Jesus.Fernando@aam.com>
2023-07-10 12:07:07 -0600

    Arreglos

---
+++ Arreglos/Arreglos/Circulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    public class Circulo
+    {
+        public int radio;
+        public Circulo(int radio) 
+        { 
+            this.radio = radio;
+        }
+    }
+}
---
+++ Arreglos/Arreglos/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+namespace Arreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numeros = new int[4];
+            int[] numeros = { 12, 34, 543, 3};
+            //Circulo[] circulos = new Circulo[10];
+            Circulo[] circulos = { new Circulo(30), new Circulo(15), new Circulo(25)};
+
+            for (int i = 0; i < circulos.Length; i++)
+            {
+                Console.WriteLine(circulos[i].radio);
+            }
+
+            foreach (Circulo circulo in circulos) 
+            {
+                Console.WriteLine($"El radio de este circulo es: {circulo.radio}");
+            }
+
+            var circulos2 = Inicializar();
+            Console.WriteLine(circulos2[2]);
+        }
+
+        static void GenerarReporte(Circulo[] circulos)
+        {
+
+        }
+
+        static int[] Inicializar()
+        {
+            Console.WriteLine("Numero de circulos: ");  
+            string entrada = Console.ReadLine();
+            int cantidad = int.Parse(entrada);
+
+            int[] circulos = new int[cantidad];
+
+           for(int i = 0;i < circulos.Length; i++)
+            {
+                Console.WriteLine("Radio para el circulo: ");
+                entrada = Console.ReadLine();
+                int radio = int.Parse(entrada);
+
+                circulos[i] = radio;    
+            }
+
+           return circulos;
+
+        }
+    }
+}
