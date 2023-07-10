53b62157fb81b2865792738568b232ffe7a0ab04
Fernando <Jesus.Fernando@aam.com>
2023-07-10 10:41:37 -0600

    Structs

---
+++ Structure/Structure/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure
+{
+    struct StructPoint
+    {
+        public int x;
+        public int y;
+
+    
+    }
+
+    class ClassPoint
+    {
+        public int x;  
+        public int y;
+    }
+
+}
---
+++ Structure/Structure/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Structure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*StructPoint s1 = new StructPoint();
+            StructPoint s2 = s1;*/
+
+            ClassPoint s1 = new ClassPoint();
+            ClassPoint s2 = s1;
+
+            Console.WriteLine(s1.x);
+            Console.WriteLine(s2.x);
+
+            s1.x++;
+            Console.WriteLine(s1.x);
+            Console.WriteLine(s2.x);
+
+        }
+
+    }
+}
