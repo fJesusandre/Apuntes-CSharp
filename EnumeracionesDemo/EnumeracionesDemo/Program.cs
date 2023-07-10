9a17b1ac16b3bb8e5699e0664f68239b2187d1eb
Fernando <Jesus.Fernando@aam.com>
2023-07-10 09:25:04 -0600

    enumeraciones

---
+++ EnumeracionesDemo/EnumeracionesDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EnumeracionesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rol tipoUsuario = Rol.Administrador;
+
+            if(tipoUsuario == Rol.Administrador)
+            {
+                tipoUsuario++;
+            }
+
+            //Rol? UsuarioNulo = null;
+
+            string valorTUsuarioGuardado = tipoUsuario.ToString();
+
+            switch (tipoUsuario)
+            {
+                case Rol.Administrador:
+                    Console.WriteLine(tipoUsuario);
+                    break;
+                case Rol.Desarrollador:
+                    Console.WriteLine(tipoUsuario);
+                    break;
+                case Rol.UsuarioFinal:
+                    Console.WriteLine(tipoUsuario);
+                    break;
+            }
+        }
+    }
+}
---
+++ EnumeracionesDemo/EnumeracionesDemo/Rol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeracionesDemo
+{
+    enum Rol : short //Eligiendo el tipo de dato del enum
+    {
+        //Por defecto se inicia en 0
+        Administrador, // = 3, se puede modificar el numero del indice de los elementos del enum
+        Desarrollador,
+        UsuarioFinal
+
+    }
+}
